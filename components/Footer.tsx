e519fc5dc262ddc27851c9615095320c600e5ed2
KidKat <kidkat.ly@icloud.com>
2024-06-11 13:37:02 +0700

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa6";
+import { socialMedia } from "@/data";
+
+const Footer = () => {
+  return (
+    <footer className="w-full mb-[100px] pb-10" id="contact">
+      <div className="w-full absolute left-0 bottom-0 min-h-96">
+        <img
+          src="/footer-grid.svg"
+          alt="grid"
+          className="w-full h-full opacity-50"
+        />
+      </div>
+
+      <div className="flex flex-col items-center">
+        <h1 className="heading lg:max-w-[45vw]">
+          Let&apos;s Build Something{" "}
+          <span className="text-purple">Amazing</span> Together
+        </h1>
+        <p className="text-white-200 md:mt-10 my-5 text-center">
+          I&apos;m always excited to collaborate on new projects. Get in touch
+          to discuss your ideas and see how we can make them a reality.
+        </p>
+        <a href="mailto:kidkat@icloud.com">
+          <MagicButton
+            title="Let's get in touch!"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
+      </div>
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+        <p className="text-base md:text-sm md:font-normal font-light">
+          Copyright © 2024 Gunawan
+        </p>
+        <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map((profile) => (
+            <a
+              key={profile.id}
+              href={profile.link}
+              target="_blank"
+              rel="noreferrer"
+              className="size-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            >
+              <img
+                src={profile.img}
+                alt={profile.name}
+                width={20}
+                height={20}
+              />
+            </a>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ data/index.ts
@@ -0,0 +1,151 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Gunawan Siswo Kuncoro",
+    description: "Undergraduate Computer Science Student",
+    className:
+      "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh] shadow-gradient",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/myprofile.webp",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Learning and Developing Various Projects.",
+    description: "Gaining Real-World Experience",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Artist & Designer Portfolio Website",
+    des: "A curated platform for designers and artists to promote their work, fostering innovation. Allows creators to share their work, network, and find inspiration through a unified interface.",
+    img: "/p1.png",
+    iconLists: ["/re.svg", "/tail.svg", "/sanity.svg"],
+    link: "https://github.com/reikki7/artify",
+  },
+  {
+    id: 2,
+    title: "Customizable 3D Product Showcase",
+    des: "Designed for creative customization. It provides a user-friendly interface for styling and personalizing images with various decals, colors, and filters.",
+    img: "/p2.png",
+    iconLists: ["/re.svg", "/tail.svg", "/three.svg"],
+    link: "https://github.com/adrianhajdin/zoom-clone",
+  },
+  {
+    id: 3,
+    title: "Android Ecommerce App",
+    des: "A seamless shopping experience tailored for Android users. Discover a diverse array of products, easily navigate categories, and secure transactions.",
+    img: "/p3.png",
+    iconLists: ["/firebase.svg", "/java.svg"],
+    link: "https://github.com/reikki7/android-ecommerce-app",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.webp",
+    iconLists: ["/re.svg", "/tail.svg", "/three.svg", "/gsap.svg"],
+    link: "https://github.com/reikki7/apple-phone-15-pro-showcase",
+  },
+];
+
+// export const workExperience = [
+//   {
+//     id: 1,
+//     title: "Frontend Engineer Intern",
+//     desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+//     className: "md:col-span-2",
+//     thumbnail: "/exp1.svg",
+//   },
+//   {
+//     id: 2,
+//     title: "Mobile App Dev - JSM Tech",
+//     desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+//     className: "md:col-span-2",
+//     thumbnail: "/exp2.svg",
+//   },
+//   {
+//     id: 3,
+//     title: "Freelance App Dev Project",
+//     desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+//     className: "md:col-span-2",
+//     thumbnail: "/exp3.svg",
+//   },
+//   {
+//     id: 4,
+//     title: "Lead Frontend Developer",
+//     desc: "Developed and maintained user-facing features using modern frontend technologies.",
+//     className: "md:col-span-2",
+//     thumbnail: "/exp4.svg",
+//   },
+// ];
+
+export const socialMedia = [
+  {
+    id: 1,
+    name: "github",
+    img: "/git.svg",
+    link: "https://github.com/reikki7",
+  },
+  {
+    id: 2,
+    name: "linkedin",
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/gunawan-siswo-kuncoro/",
+  },
+];
